d32f976a63241b2f0789fd57debd1d89aa9dab16
Rocko Bauman <jackrockocasey@gmail.com>
2024-05-20 11:17:57 -0400

    update styling

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(∏
-  document.getElementById("root") as HTMLElement
+const root = ReactDOM.createRoot(
+  global.document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
